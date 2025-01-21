0c88a681a1f9d9b57ee38250be9226bd8d099f88
MiloszNejbauer <milosznejbauer@gmail.com>
2025-01-21 21:26:35 +0100

    render updates

--- src/main/java/com/example/demo/HttpToHttpsRedirectConfig.java
+++ src/main/java/com/example/demo/HttpToHttpsRedirectConfig.java
@@ -1,4 +1,28 @@
-package com.example.demo;
-
-public class HttpToHttps {
-}
+//package com.example.demo;
+//
+//import org.apache.catalina.connector.Connector;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+//import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+//import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+//
+//@Configuration
+//public class HttpToHttpsRedirectConfig implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+//
+//    @Override
+//    public void customize(ConfigurableServletWebServerFactory factory) {
+//        if (factory instanceof TomcatServletWebServerFactory) {
+//            ((TomcatServletWebServerFactory) factory)
+//                    .addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+//        }
+//    }
+//
+//    private Connector httpToHttpsRedirectConnector() {
+//        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+//        connector.setScheme("http");
+//        connector.setPort(8080); // Port HTTP
+//        connector.setSecure(false);
+//        connector.setRedirectPort(8443); // Port HTTPS
+//        return connector;
+//    }
+//}
