7d0252408eeab3ca38fad66d4d7b26fe892b896e
MiloszNejbauer <milosznejbauer@gmail.com>
2025-01-21 20:22:14 +0100

    Fix 2

--- demo/src/main/java/com/example/demo/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
-
-public interface TransactionRepository extends MongoRepository<Transaction, String> {
-    List<Transaction> findByUserId(String userId);
-}
